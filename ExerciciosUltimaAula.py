4aed5d278b3beacf59db09590834b0ac7cb8cd60
lianarapistt <4553323108@estudante.sed.sc.gov.br>
2025-11-17 21:21:23 -0300

    Update and rename atividade.10.11.py to ExerciciosUltimaAula.py

--- ExerciciosUltimaAula.py
+++ ExerciciosUltimaAula.py
@@ -37,7 +37,7 @@ def soma_digitos(numero):
     return soma_total
 
 # Função: Palíndromo
-def eh_palindromo(text):
+def verificar_palindromo(text):
     t_sem_espaco = text.replace(" ", "")
     t_limpo = t_sem_espaco.lower()
     t_invertido = t_limpo[::-1]  
@@ -61,28 +61,3 @@ def media_lista(l):
     return soma / quantidade
 
 
-# Função: Média dos Elementos 
-print("--- 1. Primos no Intervalo (10 a 20) ---")
-print(f"Resultado: {primos_intervalo(10, 20)}") # retornar [11, 13, 17, 19]
-
-print("\n--- 2. Ordenar e Remover Duplicatas ---")
-lista_com_duplicatas = [5, 2, 8, 2, 5, 1, 8]
-print(f"Lista Original: {lista_com_duplicatas}")
-print(f"Resultado: {ordenar_sem_repeticao(lista_com_duplicatas)}") #retornar [1, 2, 5, 8]
-
-print("\n--- 3. Soma dos Dígitos ---")
-print(f"Soma dos dígitos de 456: {soma_digitos(456)}") # retornar 4 + 5 + 6 = 15
-
-print("\n--- 4. Palíndromo ---")
-frase_palindromo = "A base do teto desaba"
-print(f"'{frase_palindromo}' é palíndromo? {eh_palindromo(frase_palindromo)}") #retornar True
-
-print("\n--- 5. Frequência de Palavras ---")
-texto_exemplo = "Eu amo programar, e você ama programar também"
-print(f"Frequência: {frequencia_palavras(texto_exemplo)}")
-#retornar {'eu': 1, 'amo': 2, 'programar,': 2, 'e': 1, 'você': 1, 'também': 1}
-
-print("\n--- 6. Média dos Elementos ---")
-lista_numeros = [10, 20, 30, 40]
-print(f"Média de {lista_numeros}: {media_lista(lista_numeros)}") # retornar 100 / 4 = 25.0
-print(f"Média de lista vazia: {media_lista([])}") # retornar None
